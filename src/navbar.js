c8937ae119dc124f11cb8affe62d866f78117b01
Uzonna Alexander <uzonna101@outlook.com>
2025-05-09 18:40:39 -0400

    Centered Div

--- src/navbar.js
+++ src/navbar.js
@@ -1,7 +1,6 @@
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-            <h1>Uzonna Alexander</h1>
             <div className="links">
                 <a href="/" >Home</a>
                 <a href="/about" >About</a>
