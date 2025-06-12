502883226959c66b91c4765da2eb618e37070c40
Uzonna Alexander <uzonna101@outlook.com>
2025-06-12 12:08:25 -0400

    Added DOCX Resume

--- src/Components/NavBar/ResumeButton.js
+++ src/Components/NavBar/ResumeButton.js
@@ -5,7 +5,7 @@ function ResumeButton() {
   return (
     <Link
       className="navItem resumeButton"
-      to="/uzonna_alexander_resume.pdf"
+      to="/uzonna_alexander_resume.docx"
       target="_blank"
     >
       <h3>Resume</h3>
